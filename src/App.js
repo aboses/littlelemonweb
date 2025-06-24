cecbabbd73e5541753e5d4b9a02fec00e0634445
aboses <arpitaboseshilpi@gmail.com>
2025-06-24 18:05:57 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import './App.css';
-
+import Header from './Header';
+import Navigation from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 function App() {
-  return(
-    <div className="App">
-      <header className="App-header">
-        <h1>Little Lemon</h1>
-        <p>Welcome to our restaurant!</p>
-      </header>
-      <main>
-        <section>
-          <h2>About Us</h2>
-          <p>We serve delicious food with a touch of love.</p>
-        </section>
-        <section>
-          <h2>Menu</h2>
-          <p>Check out our amazing dishes!</p>
-        </section>
-      </main>
-      <footer>
-        <p>&copy; 2023 Little Lemon. All rights reserved.</p>
-      </footer>
-    </div>
+  return (
+    <>
+      <Header/>
+      <Navigation/>
+      <Main/>
+      <Footer/>
+    </>
   )
 }
+
 
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Main= () => {
+  return (
+    <>
+        <section>
+            <h2>About Us</h2>
+            <p>We serve delicious food with a touch of love.</p>
+        </section>
+        <section>
+            <h2>Menu</h2>
+            <p>Check out our amazing dishes!</p>
+        </section>
+    </>
+
+  )
+}
+
+
+export default Main;
